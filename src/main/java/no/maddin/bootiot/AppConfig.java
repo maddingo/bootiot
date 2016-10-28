c9cae73f77c17ddb5f7d5a2da89e51d67491b98a
Martin Goldhahn <mgoldhahn@gmail.com>
2016-10-28 03:00:31 +0200

    start processing request params

--- src/main/java/no/maddin/bootiot/AppConfig.java
+++ src/main/java/no/maddin/bootiot/AppConfig.java
@@ -1,7 +1,9 @@
 package no.maddin.bootiot;
 
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @ServletComponentScan
+@Configuration
 public class AppConfig {
 }
