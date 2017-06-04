a146dcce8c13f39d59cc339f754a6aa644795db5
Martin Goldhahn <mgoldhahn@gmail.com>
2017-06-05 01:20:50 +0200

    improved error handling

--- src/main/java/no/maddin/bootiot/TwilioInputServlet.java
+++ src/main/java/no/maddin/bootiot/TwilioInputServlet.java
@@ -1,8 +1,14 @@
 package no.maddin.bootiot;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.twilio.twiml.Body;
+import com.twilio.twiml.Message;
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.TwiMLException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Arrays;
 import java.util.Map;
@@ -48,22 +55,27 @@ public class TwilioInputServlet extends HttpServlet {
     @Autowired
     private EntryStore entryStore;
 
-    @Value("${TWILIO_SERVICE_SID}")
+    @Value("${TWILIO_SERVICE_SID:T(java.util.UUID).randomUUID().toString()}")
     private String serviceSid;
 
-    @Value("${TWILIO_ACCOUNT_SID}")
+    @Value("${TWILIO_ACCOUNT_SID:T(java.util.UUID).randomUUID().toString()}")
     private String accountSid;
+
+    @Value("${ERROR_SMS_RECEIVER:}")
+    private String errorMessageReceiver;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        checkCredentials(req.getParameterMap());
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        checkCredentials(parameterMap);
         Writer writer = resp.getWriter();
 
-        saveEntry(req.getParameterMap());
-
-        sendResponse(resp, writer);
-
-        logParameters(req);
+        try {
+            saveEntry(parameterMap);
+            sendSuccessResponse(resp, writer, parameterMap);
+        } catch (IllegalArgumentException ex) {
+            sendErrorResponse(resp, writer, parameterMap, ex);
+        }
     }
 
     private void saveEntry(Map<String, String[]> parameters) {
@@ -71,10 +83,15 @@ public class TwilioInputServlet extends HttpServlet {
         if (bodyValue != null && !bodyValue.isEmpty()) {
             String json = bodyValue.replace('<', '{').replace('>','}');
             entryStore.saveJson(json);
+        } else {
+            throw new IllegalArgumentException("empty body");
         }
     }
 
     private void checkCredentials(Map<String, String[]> parameters) throws ServletException {
+        if (StringUtils.isEmpty(accountSid) || StringUtils.isEmpty(serviceSid)) {
+            throw new ServletException("invalid configuration");
+        }
         if (!accountSid.equals(parameters.get("AccountSid")[0]) ||
                 !serviceSid.equals(parameters.get("MessagingServiceSid")[0]))
         {
@@ -82,18 +99,48 @@ public class TwilioInputServlet extends HttpServlet {
         }
     }
 
-    private void sendResponse(HttpServletResponse resp, Writer writer) throws IOException {
-        resp.setContentType("text/xml");
-        writer.append("<Response/>");
+    private void sendSuccessResponse(HttpServletResponse resp, Writer writer, Map<String, String[]> parameterMap) throws IOException {
+        resp.setContentType("application/xml");
+        try {
+            String respString = new MessagingResponse.Builder().build().toXml();
+            writer.append(respString);
+        } catch (TwiMLException e) {
+            throw new IOException(e);
+        }
+//        writer.append("<Response/>");
     }
 
-    private void logParameters(HttpServletRequest req) {
-        String paramString = req.getParameterMap()
+    private void sendErrorResponse(HttpServletResponse resp, Writer writer, Map<String, String[]> parameterMap, Throwable tr) throws IOException {
+        resp.setContentType("text/xml");
+
+        MessagingResponse.Builder b = new MessagingResponse.Builder();
+        if (!StringUtils.isEmpty(errorMessageReceiver)) {
+            b.message(
+              new Message.Builder()
+                      .to(errorMessageReceiver)
+                      .body(new Body("Received erroneous message: Check message log at https://papertrailapp.com/dashboard"))
+                      .build()
+            );
+        }
+
+        StringWriter errorRequestString = new StringWriter();
+        logParameters(parameterMap, errorRequestString);
+        log.error("Received erroneous message: " + errorRequestString.toString());
+        try {
+            writer.append(b.build().toXml());
+        } catch (TwiMLException e) {
+            throw new IOException(e);
+        }
+    }
+
+    private void logParameters(Map<String, String[]> parameterMap, Writer writer) throws IOException {
+
+        String paramString = parameterMap
                 .entrySet()
                 .stream()
                 .map(entry -> entry.getKey() + ": " + Arrays.toString(entry.getValue()))
                 .collect(java.util.stream.Collectors.joining(", "));
 
-        log.info("RequestParameters: " + paramString);
+        writer.append("RequestParameters: ").append(paramString);
     }
 }
